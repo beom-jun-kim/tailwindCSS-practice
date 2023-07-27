85b5d373f64be1b96da71333be9641d8176dbb39
kimbeomjun <gimb52043@gmail.com>
2023-07-27 17:39:53 +0900

    라이브 방송 페이지

---
+++ app/live/[id]/page.tsx
@@ -0,0 +1,89 @@
+import type { NextPage } from "next";
+
+const Stream: NextPage = () => {
+  return (
+    <div className="py-10 px-4 space-y-4">
+      <div className="w-full rounded-md shadow-sm bg-slate-300 aspect-video" />
+      <div className="mt-5">
+        <h1 className="text-3xl font-bold text-gray-900">맥북에어 m1 256g (매직마우스 포함)</h1>
+        <span className="text-2xl block mt-3 text-gray-900">$1400</span>
+        <p className=" my-6 text-gray-700">
+        못할 미묘한 과실이 구하지 청춘을 얼마나 천고에 청춘이 같이, 그리하였는가? 
+        행복스럽고 내려온 영원히 보배를 아름다우냐? 품고 시들어 보이는 별과 것은 힘있다. 
+        오아이스도 얼음 목숨이 자신과 있는 운다. 같은 쓸쓸한 사는가 위하여 창공에 운다. 
+        청춘 끝까지 그들의 인간이 돋고, 속잎나고, 군영과 맺어, 운다. 
+        그들은 청춘의 미인을 봄바람이다. 품에 날카로우나 동산에는 인생을 긴지라 칼이다. 
+        그들은 이상은 그들의 보내는 것이다. 피고, 청춘이 그들에게 품고 그들의 노년에게서 것이 뿐이다.
+        </p>
+      </div>
+      <div>
+        <h2 className="text-2xl font-bold text-gray-900">Live Chat</h2>
+        <div className="py-10 pb-16 h-[50vh] overflow-y-scroll  px-4 space-y-4">
+          <div className="flex items-start space-x-2">
+            <div className="w-8 h-8 rounded-full bg-slate-400" />
+            <div className="w-1/2 text-sm text-gray-700 p-2 border border-gray-300 rounded-md">
+              <p>5,000원만 네고해주시면 안될까요?</p>
+            </div>
+          </div>
+          <div className="flex flex-row-reverse items-start space-x-2 space-x-reverse">
+            <div className="w-8 h-8 rounded-full bg-slate-400" />
+            <div className="w-1/2 text-sm text-gray-700 p-2 border border-gray-300 rounded-md">
+              <p>3,000원까지 하겠습니다</p>
+            </div>
+          </div>
+          <div className="flex items-start space-x-2">
+            <div className="w-8 h-8 rounded-full bg-slate-400" />
+            <div className="w-1/2 text-sm text-gray-700 p-2 border border-gray-300 rounded-md">
+              <p>그럼 어쩔 수 없네요</p>
+            </div>
+          </div>
+          <div className="flex flex-row-reverse items-start space-x-2 space-x-reverse">
+            <div className="w-8 h-8 rounded-full bg-slate-400" />
+            <div className="w-1/2 text-sm text-gray-700 p-2 border border-gray-300 rounded-md">
+              <p>죄송하지만 더 이상은 힘들어요ㅜㅜ</p>
+            </div>
+          </div>
+          <div className="flex items-start space-x-2">
+            <div className="w-8 h-8 rounded-full bg-slate-400" />
+            <div className="w-1/2 text-sm text-gray-700 p-2 border border-gray-300 rounded-md">
+              <p>저 살래요!</p>
+            </div>
+          </div>
+          <div className="flex flex-row-reverse items-start space-x-2 space-x-reverse">
+            <div className="w-8 h-8 rounded-full bg-slate-400" />
+            <div className="w-1/2 text-sm text-gray-700 p-2 border border-gray-300 rounded-md">
+              <p>네 거래는 언제 어디서 하실 수 있죠?</p>
+            </div>
+          </div>
+          <div className="flex items-start space-x-2">
+            <div className="w-8 h-8 rounded-full bg-slate-400" />
+            <div className="w-1/2 text-sm text-gray-700 p-2 border border-gray-300 rounded-md">
+              <p>서울 특별시 동작구 xx아파트 앞입니다</p>
+            </div>
+          </div>
+          <div className="flex flex-row-reverse items-start space-x-2 space-x-reverse">
+            <div className="w-8 h-8 rounded-full bg-slate-400" />
+            <div className="w-1/2 text-sm text-gray-700 p-2 border border-gray-300 rounded-md">
+              <p>네 오후 5시까지 가겠습니다</p>
+            </div>
+          </div>
+        </div>
+        <div className="fixed py-2 bg-white  bottom-0 inset-x-0">
+          <div className="flex relative max-w-md items-center  w-full mx-auto">
+            <input
+              type="text"
+              className="shadow-sm rounded-full w-full border-gray-300 focus:ring-orange-500 focus:outline-none pr-12 focus:border-orange-500"
+            />
+            <div className="absolute inset-y-0 flex py-1.5 pr-1.5 right-0">
+              <button className="flex focus:ring-2 focus:ring-offset-2 focus:ring-orange-500 items-center bg-orange-500 rounded-full px-3 hover:bg-orange-600 text-sm text-white">
+                &rarr;
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Stream;
